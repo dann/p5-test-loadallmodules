5b0054685537dfbb42c4a392851c17aef5f50a6d
Dann <techmemo@gmail.com>
2009-02-24 18:30:49 +0900

    initial commit

---
+++ lib/Test/LoadAllModules.pm
@@ -0,0 +1,33 @@
+package Test::LoadAllModules;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Test::LoadAllModules -
+
+=head1 SYNOPSIS
+
+  use Test::LoadAllModules;
+
+=head1 DESCRIPTION
+
+Test::LoadAllModules is
+
+=head1 AUTHOR
+
+Takatoshi Kitano E<lt>kitano.tk@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
